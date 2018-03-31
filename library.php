5ee208d1f3a7a702e891889f8c644400a7cd2e75
Joshua <jy2xj@virginia.edu>
2018-03-31 17:08:54 -0400

    Get search page working

---
+++ library.php
@@ -0,0 +1,6 @@
+<?php
+ $SERVER = 'stardock.cs.virginia.edu';
+ $USERNAME = 'CS4750jy2xja';
+ $PASSWORD = 'spring2018';
+ $DATABASE = 'CS4750jy2xj';
+?> 
--- search.php
+++ search.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <link type="text/css" rel="stylesheet" href="stylesheet2.css"/>
     <title>Cville Foodies | Home</title>
 
     <!-- Latest compiled and minified CSS -->
@@ -18,10 +19,10 @@
  
   <!-- Navbar -->
   <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-    <a href="#" class="pull-left">
+    <a href="./" class="pull-left">
         <img src="./images/logo.png">
     </a> 
-    <a href="#" class="navbar-brand">Cville Foodies</a>
+    <a href="./" class="navbar-brand">Cville Foodies</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
@@ -32,7 +33,7 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="./about.html">About</a>
+        <a class="nav-link" href="#">About</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Categories</a>
@@ -47,13 +48,61 @@
 </nav>
 
   <body id="homepage">
-      <div class="container">
+      <div class="container" id="searchResultBar">
         <form class="searchBar" action="search.php" method="post">
             <input type="text" placeholder="Find burgers, pasta, bars.." name="search">
             <button type="submit"><i class="fa fa-search"></i></button>
+            <button type="button" id="filterButton"><i class="fa">FILTER</i></button>
         </form>
       </div>
+
+      <ul class="list">
+        <?php
+           require_once('./library.php');
+           $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
+           // Check connection
+           if (mysqli_connect_errno()) {
+           echo("Can't connect to MySQL Server. Error code: " .
+          mysqli_connect_error());
+           return null;
+           }
+           
+          if (!isset($_POST["search"]) || empty($_POST["search"])) {
+            echo "<li> No Search Results found!</li>";
+            exit;
+          }
+           // Form the SQL query (a SELECT query)
+           $sql="SELECT *
+            FROM restaurant
+            WHERE rname
+            LIKE
+            '%{$_POST['search']}%'";
+           $result = mysqli_query($con,$sql);
+           // Print the data from the table row by row
+           $i = 1;
+           if (mysqli_num_rows($result)==0) { 
+            echo "<li> No Search Results found!</li>";
+            exit;
+          }
+           while($row = mysqli_fetch_array($result)) {
+            $tableRow = "<li>
+              <div class=\"outerDiv\">
+                <div class=\"leftCol\">
+                  <h2 class=\"rName\">$i. {$row['rname']}</h2>
+                  <p class=\"rInfo\"><span class=\"rCost\">{$row['price_range']}</span> <span class=\"rReview\">{$row['num_of_reviews']} Reviews</span></p>
+                </div>
+                <div class=\"rightCol\">
+                  <p>{$row['street']}, Charlottesville, VA</p>
+                  <p>{$row['phone_num']}</p>
+                </div>
+              </div>
+            </li>";
+             echo $tableRow;
+             $i++;
+           }
+           mysqli_close($con);
+        ?> 
+      </ul>
   </body>
 
-<footer> &copy; Copyright 2018 - Katherine Qian, Jodie Ryu, Youbeen Shim, Joshua Ya. All rights reserved.
 </html>
